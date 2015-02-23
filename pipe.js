9530d2f2662decc0075dc831db266d758f0488b9
mihirgokani007 <mihirgokani007@yahoo.co.in>
2015-02-23 21:35:31 +0530

    Initial commit

---
+++ pipe.js
@@ -0,0 +1,124 @@
+/**
+
+DESCRIPTION:
+  
+  Async tasks queue. 
+
+
+EXAMPLE:
+
+  var task_add = function(argx, argy, done) { 
+    setTimeout(function() {
+      console.log('add complete!', argx, argy); 
+      done(null, argx + argy);
+    }, 5000);
+  }
+  var task_sub = function(argx, argy, done) { 
+    setTimeout(function() {
+      console.log('sub complete!', argx, argy); 
+      done(null, argx - argy);
+    }, 1000);
+  }
+
+  var pipe = new Pipe();
+  pipe
+    .enqueue(task_add, 10, 15)
+    .enqueue(task_sub, 40, 20)
+    .dequeue(function(err, result) { 
+      console.log('result', result);
+    })
+    .dequeue(function(err, result) { 
+      console.log('result', result);
+    });
+
+**/
+
+(function() {
+  var g = this;
+  var slice = [].slice;
+
+  function Pipe(concurrency /*, tasks*/) {
+    
+    // maximum number of concurrent tasks
+    this._c = concurrency || 1;
+    // pending tasks queue
+    this._t = slice.call(arguments, 1);
+    // queue of callbacks awaiting for tasks
+    this._w = [];
+    // number of running tasks (always <= this._c)
+    this._r = 0;
+
+  }
+
+  Pipe.prototype = {
+
+    /**
+     * Enqueue a new task to the tasks queue.
+     */
+    fill: function(fn) {
+      var task = {fn: fn, args: slice.call(arguments, 1)};
+      // if any tasks already available in the queue
+      if (!this._w.length || this._r >= this._c) {
+        // append [fn + arguments] to tasks queue
+        this._t.push(task);
+        return this;
+      } 
+      // get done callback
+      var done = this._w.shift();
+      // start task
+      return this._run(task, done);
+    },
+
+    /**
+     * Dequeue a task from the tasks queue and execute it.
+     */
+    fetch: function(cb, cxt) {
+      var done = {cb: cb, cxt: cxt || this};
+      // if any callbacks already waiting in the queue
+      if (!this._t.length || this._r >= this._c) {
+        // wait for availability of task in the queue
+        this._w.push(done);
+        return this; 
+      }
+      // get [fn + its arguments]
+      var task = this._t.shift();
+      // start task
+      return this._run(task, done);
+    },
+
+    _run: function(task, done) {
+      // task is running, increment count
+      ++this._r;
+      
+      // add done callback to task args
+      task.args.push(function(err, val) { 
+        // task completed, decrement count
+        --this._r;
+        // notify via done callback
+        done.cb.call(done.cxt, err, val)
+        // if any pending tasks in task queue and
+        // there's a request to run the task and
+        // not many tasks are already running
+        if (this._t.length && this._w.length && (this._r < this._c)) {
+          // reuse variables for next runnable task
+          task = this._t.shift();
+          done = this._w.shift();
+          // run the next runnable task
+          this._run(task, done);
+        }
+      }.bind(this));
+
+      // call task function and wait for it to call done
+      task.fn.apply(null, task.args); 
+      
+      // chain
+      return this;
+    }
+
+  };
+
+  if (typeof define === "function" && define.amd) define(function() { return Pipe; });
+  else if (typeof module === "object" && module.exports) module.exports = Pipe;
+  else this.Pipe = Pipe;
+  
+}());
